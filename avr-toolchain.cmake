68e83e1adbde05c5dad2e62830233f4f62e1e0e9
Tropby <Tropby@gmx.net>
2022-04-24 22:48:10 +0200

    Insert compiler at the front of the program path list.

--- avr-toolchain.cmake
+++ avr-toolchain.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.0.0)
 
-list(APPEND CMAKE_PROGRAM_PATH "${CMAKE_CURRENT_LIST_DIR}/toolchain/bin")
+list(INSERT CMAKE_PROGRAM_PATH 0 "${CMAKE_CURRENT_LIST_DIR}/toolchain/bin")
 
 set(CMAKE_C_COMPILER "${CMAKE_CURRENT_LIST_DIR}/toolchain/bin/avr-gcc.exe")
 set(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_LIST_DIR}/toolchain/bin/avr-g++.exe")
