30ed801c1e525d6e5429ff7813ceb103ed5287e4
Carsten <Tropby@gmx.net>
2025-09-03 20:38:53 +0200

    Setup toolchain file

--- avr-toolchain.cmake
+++ avr-toolchain.cmake
@@ -1,8 +1,17 @@
-cmake_minimum_required(VERSION 3.0.0)
+cmake_minimum_required(VERSION 3.5.0)
+
+set(CMAKE_SYSTEM_NAME       Generic         )
+set(CMAKE_SYSTEM_PROCESSOR  avr             )
+
 
 list(INSERT CMAKE_PROGRAM_PATH 0 "${CMAKE_CURRENT_LIST_DIR}/toolchain/bin")
 
 set(CMAKE_C_COMPILER "${CMAKE_CURRENT_LIST_DIR}/toolchain/bin/avr-gcc.exe")
 set(CMAKE_CXX_COMPILER "${CMAKE_CURRENT_LIST_DIR}/toolchain/bin/avr-g++.exe")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 include_directories("${CMAKE_CURRENT_LIST_DIR}/toolchain/avr/include")
